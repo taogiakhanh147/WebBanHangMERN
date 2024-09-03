dd904923f00a6dd0828b7fb91f376fc5b1b14dda
TaoGiaKhanh147 <taogiakhanh@gmail.com>
2024-09-03 15:34:17 +0700

    update5

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -14,9 +14,9 @@ const CardComponent = () => {
   return (
     <WrapperCardStyle
       hoverable
-      headStyle={{width: '200px', height: '200px'}}
+      // headStyle={{width: '200px', height: '200px'}}
       style={{ width: 180, borderRadius: '0 0 5px 5px'}}
-      bodyStyle={{ padding: "10px" }}
+      // bodyStyle={{ padding: "10px" }}
       cover={
         <img
           alt="example"
